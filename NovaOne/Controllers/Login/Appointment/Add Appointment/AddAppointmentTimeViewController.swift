a42932e6bb89cccc649e08b45669bb7f1a78f729
Andre Mashraghi <andre.mashraghi@gmail.com>
2020-06-08 09:36:20 -0400

    Added the ability to restrict past times and available times in the appointment time picker

--- NovaOne/Controllers/Login/Appointment/Add Appointment/AddAppointmentTimeViewController.swift
+++ NovaOne/Controllers/Login/Appointment/Add Appointment/AddAppointmentTimeViewController.swift
@@ -12,6 +12,8 @@ class AddAppointmentTimeViewController: AddAppointmentBaseViewController {
     
     // MARK: Properties
     @IBOutlet weak var appointmentTimePicker: UIDatePicker!
+    var hoursEnabled: [Int]?
+    var daysEnabled: [Int]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,36 +22,70 @@ class AddAppointmentTimeViewController: AddAppointmentBaseViewController {
     
     func setupTimePicker() {
         // Sets up the time picker
+        
+        // Get company to get hours enabled and days enabled
+        guard let companyId = self.appointment?.companyId else { return }
+        let predicate = NSPredicate(format: "id = %@", String(companyId))
+        
+        // Set the minimun allowed date (starting date) for the datetime picker
+        guard
+            let company = PersistenceService.fetchEntity(Company.self, filter: predicate, sort: nil).first
+        else { return }
+        
+        self.daysEnabled = company.daysOfTheWeekEnabled?.components(separatedBy: ",").map {
+            guard let integer = Int($0) else { return 0 }
+            return integer
+        }.sorted()
+        self.hoursEnabled = company.hoursOfTheDayEnabled?.components(separatedBy: ",").map {
+            guard let integer = Int($0) else { return 0 }
+            return integer
+        }.sorted()
+        
+        // Get the next available week day and hour
+        let now = Date()
+        var nowDateComponents = Calendar.current.dateComponents([.calendar, .day, .era, .hour, .minute, .month, .nanosecond, .quarter, .second, .timeZone, .weekday, .year, .weekOfMonth], from: now)
+        
+        guard
+            let nowWeekday = nowDateComponents.weekday,
+            let nowHour = nowDateComponents.hour,
+            let closestWeekDayIndex = self.daysEnabled?.firstIndex(where: {$0 >= (nowWeekday - 1)}),
+            let closestHourIndex = self.hoursEnabled?.firstIndex(where: { $0 >= nowHour }),
+            let closestWeekDay = self.daysEnabled?[closestWeekDayIndex],
+            let closestHour = self.hoursEnabled?[closestHourIndex]
+        else { return }
+        
+        // Create date from closest date components
+        nowDateComponents.hour = closestHour
+        nowDateComponents.weekday = closestWeekDay
+        
+        let nextAvailableDate = Calendar.current.date(from: nowDateComponents)
+        
+        self.appointmentTimePicker.minimumDate = nextAvailableDate
         self.appointmentTimePicker.minuteInterval = 30
     }
     
     // MARK: Actions
     @IBAction func continueButtonTapped(_ sender: Any) {
         
-        // Get company to get hours enabled and days enabled
-        guard let companyId = self.appointment?.companyId else { return }
-        let predicate = NSPredicate(format: "id = %@", String(companyId))
-        
-        guard
-            let company = PersistenceService.fetchEntity(Company.self, filter: predicate, sort: nil).first,
-            let hoursEnabled = company.hoursOfTheDayEnabled?.components(separatedBy: ","),
-            let daysEnabled = company.daysOfTheWeekEnabled?.components(separatedBy: ",")
-        else { return }
-        print("COMPANY NAME: \(company.name!)")
-        
         // Get hour and day selected from the datetime picker to see if day and hour selected is available
         let dateSelected = self.appointmentTimePicker.date
-        let selectedweekDay = String(Calendar.current.component(.weekday, from: dateSelected) - 1)
-        let selectedhour = String(Calendar.current.component(.hour, from: dateSelected))
+        let selectedweekDay = Calendar.current.component(.weekday, from: dateSelected) - 1
+        let selectedhour = Calendar.current.component(.hour, from: dateSelected)
         print("WEEK DAY: \(selectedweekDay), HOUR: \(selectedhour)")
         
+        guard
+            let hoursEnabled = self.hoursEnabled,
+            let daysEnabled = self.daysEnabled
+        else { return }
+        
         // If the day or hour selected is NOT in the days or hours enabled array, pop up an error
+        let popUpOkViewController = self.alertService.popUpOk(title: "Unavailable Day", body: "The day selected for this company is unavailable. Please choose a different day.")
         if !daysEnabled.contains(selectedweekDay) {
-            let popUpOkViewController = self.alertService.popUpOk(title: "Unavailable Day", body: "The day selected for this company is unavailable. Please choose a different day.")
             self.present(popUpOkViewController, animated: true, completion: nil)
             return
         } else if !hoursEnabled.contains(selectedhour) {
-            let popUpOkViewController = self.alertService.popUpOk(title: "Unavailable Hour", body: "The hour selected for this company is unavailable. Please choose a different hour.")
+            popUpOkViewController.popUpTitle = "Unavailable Hour"
+            popUpOkViewController.popUpBody = "The hour selected for this company is unavailable. Please choose a different hour."
             self.present(popUpOkViewController, animated: true, completion: nil)
             return
         }
