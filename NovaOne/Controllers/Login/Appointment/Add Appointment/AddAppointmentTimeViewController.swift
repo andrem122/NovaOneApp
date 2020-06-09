69f03785b614d1c0ff8e99369cc1e05df9779566
Andre Mashraghi <andre.mashraghi@gmail.com>
2020-06-09 16:20:42 -0400

    Removed restrictions for making appointments on company schedule

--- NovaOne/Controllers/Login/Appointment/Add Appointment/AddAppointmentTimeViewController.swift
+++ NovaOne/Controllers/Login/Appointment/Add Appointment/AddAppointmentTimeViewController.swift
@@ -22,45 +22,7 @@ class AddAppointmentTimeViewController: AddAppointmentBaseViewController {
     
     func setupTimePicker() {
         // Sets up the time picker
-        
-        // Get company to get hours enabled and days enabled
-        guard let companyId = self.appointment?.companyId else { return }
-        let predicate = NSPredicate(format: "id = %@", String(companyId))
-        
-        // Set the minimun allowed date (starting date) for the datetime picker
-        guard
-            let company = PersistenceService.fetchEntity(Company.self, filter: predicate, sort: nil).first
-        else { return }
-        
-        self.daysEnabled = company.daysOfTheWeekEnabled?.components(separatedBy: ",").map {
-            guard let integer = Int($0) else { return 0 }
-            return integer
-        }.sorted()
-        self.hoursEnabled = company.hoursOfTheDayEnabled?.components(separatedBy: ",").map {
-            guard let integer = Int($0) else { return 0 }
-            return integer
-        }.sorted()
-        
-        // Get the next available week day and hour
-        let now = Date()
-        var nowDateComponents = Calendar.current.dateComponents([.calendar, .day, .era, .hour, .minute, .month, .nanosecond, .quarter, .second, .timeZone, .weekday, .year, .weekOfMonth], from: now)
-        
-        guard
-            let nowWeekday = nowDateComponents.weekday,
-            let nowHour = nowDateComponents.hour,
-            let closestWeekDayIndex = self.daysEnabled?.firstIndex(where: {$0 >= (nowWeekday - 1)}),
-            let closestHourIndex = self.hoursEnabled?.firstIndex(where: { $0 >= nowHour }),
-            let closestWeekDay = self.daysEnabled?[closestWeekDayIndex],
-            let closestHour = self.hoursEnabled?[closestHourIndex]
-        else { return }
-        
-        // Create date from closest date components
-        nowDateComponents.hour = closestHour
-        nowDateComponents.weekday = closestWeekDay
-        
-        let nextAvailableDate = Calendar.current.date(from: nowDateComponents)
-        
-        self.appointmentTimePicker.minimumDate = nextAvailableDate
+        self.appointmentTimePicker.minimumDate = Date() // Disable past dates and times for the picker
         self.appointmentTimePicker.minuteInterval = 30
     }
     
